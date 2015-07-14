d7b03577ba6d5755886e9b5eefa4dc47cb55d151
Hernan Y.Ke <yuriqiao@gmail.com>
2015-07-13 23:13:11 -0400

    Day 13

--- r36.rb
+++ r36.rb
@@ -20,3 +20,52 @@ end
 
 puts greeter_p.call("Me")
 puts greeter_l.call("You")
+
+#Ex 3
+def context
+  l = lambda do
+    puts "In lambda"
+    return
+    puts "After return"
+  end
+
+  p = proc do
+    puts "In proc"
+    return
+    puts "After return"
+  end
+  l.call
+  p.call
+end
+
+
+#Ex 4
+def each_call(visit)
+  visit.call("a")
+  visit.call("b")
+  visit.call("c")
+  visit.call("d")
+  visit.call("e")
+end
+
+each_call(->(name) {puts "#{name}"})
+
+#Ex 5
+def each_call2(&visit)
+  visit.call("a")
+  visit.call("b")
+  visit.call("c")
+  visit.call("d")
+  visit.call("e")
+end
+
+each_call2 do |name|
+  puts "#{name}"
+end
+
+#Ex 6
+greeter_n = ->(name){
+  puts "#{name}"
+}
+
+puts greeter_n
