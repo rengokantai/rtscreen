123ad2ee066da4ad899143b7392a294a30b60065
Hernan Y.Ke <yuriqiao@gmail.com>
2015-07-20 23:31:43 -0400

    Day 20

--- r42.rb
+++ r42.rb
@@ -4,5 +4,25 @@ require 'csv'
 def memstats
   size = `ps -o size= #{$$}`.strip.to_i
 end
-
+#Ex 1
+put memstats
 visitors = CSV.read('x.csv', headers:true)
+visitors.count{|v|v['Geolocation'] = ~ /San/}
+put memstats
+
+#Ex 2
+put memstats
+count = 0
+CSV.foreach('visitor.csv',headers:true) do |v|
+  count += 1 if v['Geolocation'] = ~ /San/
+end
+put memstats
+
+#Ex 3
+put memstats
+count = 0
+CSV.foreach('visitor.csv',headers:true) do |csv|
+  visitors = csv.each
+  visitors.count{|v| v['Geolocation'] = ~ /San/}
+end
+put memstats
